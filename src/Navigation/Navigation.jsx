b542eacb4263b36be79bb4a4d61b2df5d688083b
cycodedconcept <cycodedconcept@gmail.com>
2024-02-26 14:40:41 +0100

    just completed navigation

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './navbar.css'
+import { Cart, Filter, Love, Not } from '../assets'
+
 
 const Navigation = () => {
   return (
     <React.Fragment>
       <nav className="navbar navbar-expand-lg p-3">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="/#">Navbar</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
@@ -13,21 +15,27 @@ const Navigation = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav m-auto">
             <li className="nav-item active">
-                <a className="nav-link">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-                <a className="nav-link">Ecommerce <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/#">Ecommerce <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-                <a className="nav-link">History <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/#">History <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-                <a className="nav-link">Notification <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/#">Notification <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-                <a className="nav-link">Account <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/#">Account <span className="sr-only">(current)</span></a>
             </li>
             </ul>
+
+            <img src={ Filter } alt="" className='mr-3'/>
+            <img src={ Not } alt="" className='mr-3'/>
+            <img src={ Love } alt="" className='mr-3'/>
+            <img src={ Cart } alt="" />
+
         </div>
         </nav>
     </React.Fragment>
---
+++ src/assets/index.jsx
@@ -0,0 +1,4 @@
+export const Cart = require('./cart.png');
+export const Filter = require('./filter.png');
+export const Love = require('./love.png');
+export const Not = require('./not.png');
