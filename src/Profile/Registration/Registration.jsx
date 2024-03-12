4a74f0af2a420826b96aee227e2a3dddcdae0c6d
cycodedconcept <cycodedconcept@gmail.com>
2024-03-12 15:22:47 +0100

    registration component ready for api integration

--- src/Profile/Registration/Registration.jsx
+++ src/Profile/Registration/Registration.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { Mlogo } from '../../assets'
 
 import './registration.css'
@@ -7,9 +11,24 @@ import './registration.css'
 const Registration = () => {
     
    const [dob, setDOB] = useState('');
+   const [phoneNumber, setPhoneNumber] = useState('');
+
+   const [password, setPassword] = useState('');
+   const [showPassword, setShowPassword] = useState(false);
+
+   const [confirmPassword, setConfirmPassword] = useState('');
+   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handleDOBChange = (e) => {
     setDOB(e.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
   };
 
   return (
@@ -24,21 +43,83 @@ const Registration = () => {
               </div>
 
               <form className='order-form custom-w-100 my-5'>
-                <div className="form-group mb-5">
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='First name'/>
+                <div className="form-group mb-4">
+                  <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='First name'/>
                 </div>
-                <div className="form-group mb-5">
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Last name'/>
+                <div className="form-group mb-4">
+                  <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Last name'/>
                 </div>
-                <div className="form-group mb-5">
-                <input type="date" id="dob" name="dob" value={dob} onChange={handleDOBChange} className="form-control" placeholder='Date of birth'/>
+                <div className="form-group mb-4">
+                  <input type="date" id="dob" name="dob" value={dob} onChange={handleDOBChange} className="form-control" placeholder='Date of birth'/>
                 </div>
-                <div className="form-group mb-5">
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='000'/>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <div style={{ marginRight: '10px', width: '100%' }}>
+                    <PhoneInput
+                        id="phone"
+                        displayInitialCountryFlag={false}
+                        international
+                        defaultCountry="NG"
+                        value={phoneNumber}
+                        onChange={setPhoneNumber}
+
+                    />
+                    </div>
+                    <div>
+                    {/* Display country code */}
+                    {phoneNumber && <span>{phoneNumber.split(' ')[0]}</span>}
+                    </div>
+                </div>
+
+                <div className="form-group mt-5 mb-3">
+                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email address'/>
+                </div>
+                <div style={{ position: 'relative' }} className='mb-4'>
+                    <input type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} className='form-control' placeholder='Password'/>
+                    <span
+                    onClick={togglePasswordVisibility}
+                    style={{
+                        position: 'absolute',
+                        top: '50%',
+                        right: '10px',
+                        transform: 'translateY(-50%)',
+                        cursor: 'pointer'
+                    }}
+                    >
+                    {showPassword ? <FontAwesomeIcon icon={faEye} style={{color: '#1410B4'}}/> : <FontAwesomeIcon icon={faEyeSlash} style={{color: '#1410B4'}}/>}
+                    </span>
+                </div>
+
+                <div style={{ position: 'relative' }} className='mb-4'>
+                    <input type={showConfirmPassword ? 'text' : 'password'} value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className='form-control' placeholder='Confirm Password'/>
+                    <span
+                    onClick={toggleConfirmPasswordVisibility}
+                    style={{
+                        position: 'absolute',
+                        top: '50%',
+                        right: '10px',
+                        transform: 'translateY(-50%)',
+                        cursor: 'pointer'
+                    }}
+                    >
+                    {showConfirmPassword ? <FontAwesomeIcon icon={faEye} style={{color: '#1410B4'}}/> : <FontAwesomeIcon icon={faEyeSlash} style={{color: '#1410B4'}}/>}
+                    </span>
+                </div>
+                <div className="form-group mb-4">
+                  <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Last name'/>
                 </div>
 
                 <div className="checkout text-center mt-5">
-                    <button className='check-btn'>Check out ₦400,000</button>
+                    <button className='reg-btn'>Get Started</button>
+
+                    <div className='reg mt-2'>
+                        <div>
+                         <p style={{color: '#848481'}}>Have an account?</p>
+                        </div>
+                        <div className='ml-2'>
+                         <button className='log-re'>Log In</button>
+                        </div>
+                    </div>
+                    
                 </div>
               </form>
           </div>
